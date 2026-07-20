9ed061be601e2c056a7454986d25af010a175101
mitantsoaharison-ship-it <mitantsoaharison@gmail.com>
2026-07-20 10:23:23 +0300

    Mise à jour acceuil

---
+++ acceuil.php
@@ -0,0 +1,22 @@
+<?php
+
+    include('./inc/function.php');
+    $liste = get_liste_produit();
+    var_dump($liste);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acceuil</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+
+    </div>
+</body>
+</html>
--- traitement-login.php
+++ traitement-login.php
@@ -3,6 +3,7 @@
     include('./inc/function.php');
     $etu = $_POST['etu'];
     if (isset($_POST['nom'])) ajout_membre($etu, $_POST['nom']);
+    session_start();
     $_SESSION['user'] = check_login($etu);
     
     header('location: acceuil.php')
